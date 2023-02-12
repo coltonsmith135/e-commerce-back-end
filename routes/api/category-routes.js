647fbaf39207c5aeaf2c73edd3ff38925aa639fb
coltonsmith135 <115854214+coltonsmith135@users.noreply.github.com>
2023-02-11 21:04:19 -0800

    finished code for category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,18 +16,42 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findByPk(req.params.id).then((categoryData) => {
+    res.json(categoryData)
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+     .then((newCategory) => {
+      res.json(newCategory)
+     })
+     .catch((err) => {
+      res.json(err)
+     })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      id: req.params.id,
+    },
+  )
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((deletedCategory) => {
+    res.json(deletedCategory)
+  })
+  .catch((err) => res.json(err))
 });
 
 module.exports = router;
